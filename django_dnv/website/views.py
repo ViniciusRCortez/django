a9384ef778f7a94f2adfb5848ce97313fb16c5b2
Vinícius Cortez <vinisicius@gmail.com>
2020-11-16 15:52:55 -0300

    created views

--- django_dnv/website/views.py
+++ django_dnv/website/views.py
@@ -1,3 +1,41 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django_dnv.website.forms import EmpregadoForm
+from django_dnv.website.models import Empregado
 
-# Create your views here.
+
+def emp(request):
+    if request.method == 'POST':
+        form = EmpregadoForm(request.POST)
+        if form.is_valid():
+            try:
+                form.save()
+                return redirect('\show')
+            except:
+                pass
+            else:
+                form = EmpregadoForm()
+                return render(request, 'index.html', {'form': form})
+
+
+def show(request):
+    empregados = Empregado.objects.all()
+    return render(request, 'show.html', {'empregados': empregados})
+
+
+def edit(request, id):
+    empregados = Empregado.objects.get(id=id)
+    return render(request, 'edit.html', {'empregados': empregados})
+
+
+def update(request, id):
+    empregados = Empregado.objects.get(id=id)
+    form = EmpregadoForm(instance=empregados)
+    if form.is_valid():
+        form.save()
+        return redirect('\show')
+    return render(request, 'edit.html', {'empregados': empregados})
+
+
+def delete(request, id):
+    empregados = Empregado.objects.get(id=id)
+    return redirect('\show')
